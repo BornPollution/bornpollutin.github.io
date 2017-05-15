02193cb7cb4df1bade6edb90fef920ffe2df216d
John LeBaron <johnrlebaron@gmail.com>
2017-05-15 14:26:08 -0600

    removed Linktunes and Plugins

--- packages.sh
+++ packages.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/firstproject debs
-dpkg-deb -bZgzip projects/linktunes debs
-dpkg-deb -bZgzip projects/linktunesplugins debs
 # dpkg-deb -bZgzip projects/<packagename> <output folder>
