3e3d3b74febe831c07cd016dcb64a711d7f45606
John LeBaron <johnrlebaron@gmail.com>
2017-05-14 20:32:00 -0600

    added LinkTunes Plugins

--- packages.sh
+++ packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/firstproject debs
 dpkg-deb -bZgzip projects/linktunes debs
+dpkg-deb -bZgzip projects/linktunesplugins debs
 # dpkg-deb -bZgzip projects/<packagename> <output folder>
