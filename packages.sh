3791c8332fc8fa3a91a047a787303137bdf78f95
John <johnrlebaron@gmail.com>
2018-12-31 17:30:12 -0700

    Added LimelightAlts

--- packages.sh
+++ packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 find . -name ".DS_Store" -delete
 dpkg-deb -bZgzip projects/LimelightAddons debs
+dpkg-deb -bZgzip projects/LimelightAlts debs
 # dpkg-deb -bZgzip projects/<packagename> <output folder>
