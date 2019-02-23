f4a0874a4a021d2f62d14edd60c9c268c98797d1
John <john@lebaron.pro>
2019-02-23 05:07:38 -0700

    Added WeatherLock12

--- packages.sh
+++ packages.sh
@@ -2,4 +2,5 @@
 find . -name ".DS_Store" -delete
 dpkg-deb -bZgzip projects/LimelightAddons debs
 dpkg-deb -bZgzip projects/LimelightAlts debs
+dpkg-deb -bZgzip projects/WeatherLock debs
 # dpkg-deb -bZgzip projects/<packagename> <output folder>
