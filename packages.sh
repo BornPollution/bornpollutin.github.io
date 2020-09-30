5920a0cf5b0bcfa54c60e46863dc156c44fdedfe
John <john@lebaron.pro>
2020-09-29 20:39:16 -0600

    added Legizmo (attempt 1)
    also removed ipas and weatherlock code

--- packages.sh
+++ packages.sh
@@ -2,5 +2,5 @@
 find . -name ".DS_Store" -delete
 dpkg-deb -bZgzip projects/LimelightAddons debs
 dpkg-deb -bZgzip projects/LimelightAlts debs
-dpkg-deb -bZgzip projects/WeatherLock debs
+dpkg-deb -bZgzip projects/Legizmo debs
 # dpkg-deb -bZgzip projects/<packagename> <output folder>
